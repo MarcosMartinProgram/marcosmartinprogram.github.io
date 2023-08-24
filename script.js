0783d8342b4cfbece6575a00a68bbab08c6eb08f
marcos <martinmarcospablo@gmail.com>
2023-08-23 23:16:42 -0300

    Completo portfolio

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let menuVisible = false;
-//Función que oculta o muestra el menu
+
 function mostrarOcultarMenu(){
     if(menuVisible){
         document.getElementById("nav").classList ="";
@@ -11,11 +11,11 @@ function mostrarOcultarMenu(){
 }
 
 function seleccionar(){
-    //oculto el menu una vez que selecciono una opcion
+    
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
-//Funcion que aplica las animaciones de las habilidades
+
 function efectoHabilidades(){
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
@@ -35,7 +35,7 @@ function efectoHabilidades(){
 }
 
 
-//detecto el scrolling para aplicar la animacion de la barra de habilidades
+
 window.onscroll = function(){
     efectoHabilidades();
 } 
@@ -44,15 +44,15 @@ window.onscroll = function(){
   var formulario = document.getElementById("mi-formulario");
 
   formulario.addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevenir el envío predeterminado del formulario
+    event.preventDefault(); 
 
-    // Enviar el formulario usando una solicitud AJAX
+    
     var xhr = new XMLHttpRequest();
     xhr.open(formulario.method, formulario.action);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4 && xhr.status === 200) {
-        // Redirigir al usuario a la página correcta después del envío
+        
         window.location.href = "https://marcosmartinprogram.github.io/#contacto";
       }
     };
